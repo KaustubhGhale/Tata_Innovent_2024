49e6531827be32c8cd164270c1456fd74559c5fe
Amey-sd <129416212+Amey-sd@users.noreply.github.com>
2024-09-16 00:45:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 import io
 from PIL import Image
 
-model1 = YOLO(r"/Models/yolo_v8/Medium/weights/best.pt")
+model1 = YOLO(r"/models/yolo_v8/Medium/weights/best.pt")
 app = Flask(__name__)
 UPLOAD_FOLDER = 'static/uploads'
 PROCESSED_FOLDER = 'static/processed'
